bbfb36999832114c012791b9529f28a57bf3b054
木云 <>
2019-07-02 14:30:10 +0800

    功能改进

--- iteration/generate_random_numbers/generate_random_numbers.php
+++ iteration/generate_random_numbers/generate_random_numbers.php
@@ -1,18 +1,19 @@
 <?php
 
 //创建不重复的随机函数  [$start 初始值]  [$total  结束值]
-function createRandom($start, $total){
+function createRandom($low, $high, $total){
     
-    if (!$start || !$total) {
+    if (!$low || !$total) {
         return false;
     }
-    for ($i=$start; $i < $total; $i++) {
-        $sequence[$i] = $i;
-    }
-    $end = $total - 1;
+
+    $sequence = range($low, $high);
+    $end = count($sequence) - 1;
     $output = array();
-    for ($i=$start; $i < $total; $i++) {
-        $num = rand($start,$end);//1
+
+    for ($i= 0; $i < $total; $i++) {
+
+        $num = rand(0,$end);//1
         $output[$i] = $sequence[$num];
         $sequence[$num] = $sequence[$end];
         $end--;
